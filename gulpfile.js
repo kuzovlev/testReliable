2c3051c767b4a2c2d8190de1befaa78076d10f1e
Дмитрий <kuzovlev.dm@gmail.com>
2019-02-21 23:33:04 +0200

    JS watcher added

--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,16 @@
-var gulp = require('gulp'),
+let gulp = require('gulp'),
     sass = require('gulp-sass'),
     watch = require('gulp-watch'),
     notify = require("gulp-notify"),
     mincss = require('gulp-cssnano'),
-    rename = require('gulp-rename');
-    //postcss = require('gulp-postcss');
-    //autoprefixer = require('autoprefixer');
-
-
+    rename = require('gulp-rename'),
+    uglify = require('gulp-uglify');
 gulp.task('sass', function () {
     return gulp.src('scss/**/*.scss')
         .pipe(sass())
         .pipe(gulp.dest('css/'))
         .pipe(notify("Changed and compiled file: <%= file.relative %>!"));
 });
-
 gulp.task('mincss', function () {
    return gulp.src('css/style.css')
        .pipe(mincss())
@@ -22,9 +18,15 @@ gulp.task('mincss', function () {
        .pipe(gulp.dest('minified'))
        .pipe(notify('Minified to <%= file.relative %>!'));
 });
-
+gulp.task('uglify', function(){
+   return gulp.src('js/js.js')
+       .pipe(uglify())
+       .pipe(gulp.dest('minified'))
+       .pipe(notify('Minified JS'));
+});
 gulp.task('watch', function () {
     console.log("Watcher started");
     gulp.watch('scss/**/*.scss', gulp.series('sass'));
-    gulp.watch('css/**/*.css', gulp.series('mincss'))
+    gulp.watch('css/**/*.css', gulp.series('mincss'));
+    gulp.watch('js/**/*.js' , gulp.series('uglify'));
 });
